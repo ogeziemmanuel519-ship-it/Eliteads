e2f75718601c917eb3eb9cecd066f8c97a29b3fb
ogeziemmanuel519-ship-it <ogeziemmanuel519@gmail.com>
2026-07-15 10:43:33 +0100

    Add app configuration for Elite Earn project

---
+++ app.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  expo: {
+    name: "Elite Earn",
+    slug: "elite-earn",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    splash: { backgroundColor: "#0F1115" },
+    android: {
+      package: "com.eliteEarn.app",
+      versionCode: 1,
+    },
+    plugins: [
+      "./plugins/withAdMobAppId",
+      [
+        "expo-build-properties",
+        { android: { kotlinVersion: "1.9.24" } },
+      ],
+    ],
+  },
+};
